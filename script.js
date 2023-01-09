44c7413cd3eb098eae59c8316b176c74b58d7374
Jaiane <blue.love02c@gmail.com>
2023-01-09 16:30:45 -0300

    Corrigindo erros

--- script.js
+++ script.js
@@ -2,45 +2,50 @@
 var input = document.querySelector("#texto-input")
 var vazio = document.querySelector("#resultado-vazio")
 var resultado = document.querySelector("#resultado")
+var botao = document.querySelector("#botao-3")
 
 
+document.getElementById('botao-1').addEventListener('click', function () {
+
+    let check = document.getElementById('texto-input').value;
+    if (check !== '') {
+
+        document.getElementById('resultado-vazio').style.display = "none"
+
+        let i = input.value.toLowerCase()
+        let substituir = i.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat")
+        return resultado.innerHTML = substituir
+
+    } else {
+        alert('Preencha o campo')
+    }
+})
+
+document.getElementById('botao-2').addEventListener('click', function () {
+
+    let check = document.getElementById('texto-input').value;
+    if (check !== '') {
+        document.getElementById('resultado-vazio').style.display = "none"
 
 
-function criptografar(){
-   document.getElementById('resultado-vazio').style.display = "none" 
-   
-   
-   let i = input.value.toLowerCase()
+        let i = input.value.toLowerCase()
+        let substituir = i.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u")
+        return resultado.innerHTML = substituir
+        
+    } else {
+        alert('Preencha o campo')
+    }
+})
 
-   let substituir = i.replaceAll("e","enter").replaceAll("i", "imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat")
+document.getElementById('botao-3').addEventListener('click', function () {
 
-   return resultado.innerHTML = substituir
-   
-}
+    let check = document.getElementById('resultado').value;
+    if (check !== '') {
+        navigator.clipboard.writeText(resultado.innerHTML)
+        alert("Mensagemn copiada!")
 
-function descriptografar(){
+    } else {
+        alert("Não há mensagem para copiar");
+    }
+})
 
-    document.getElementById('resultado-vazio').style.display = "none"
-
-    let i = input.value.toLowerCase()
-
-    let substituir = i.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u")
-    return resultado.innerHTML = substituir
-}
-
-
-function copiar(){
-    
-    navigator.clipboard.writeText(resultado.innerHTML).then(()=> {alert("Mensagemn copiada!")}).catch(() => {
-        alert("Falhou em copiar mensagem!");
-      });
-    
-}
-
-
-
-function clearContent(){
-    document.getElementById("texto-input").value='';
-    document.getElementById("resultado").value='';
-    document.getElementById('resultado-vazio').style.display = "flex"
-}
